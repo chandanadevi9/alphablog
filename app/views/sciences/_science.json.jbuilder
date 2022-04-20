128d05734eee9c778f7aa8a0c2c610cbb781cf57
chandana yanapu <chandana.yanapu@prasthana.com>
2022-04-20 17:59:33 +0530

    science

---
+++ app/views/sciences/_science.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! science, :id, :created_at, :updated_at
+json.url science_url(science, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sciences
  root 'pages#home'
  get 'about' ,to: 'pages#about'
  #get 'chandu' ,to: 'name#home'
